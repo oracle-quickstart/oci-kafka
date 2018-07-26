ad4412cb26ac7d7d45e1860ca9550ea5efdbf228
jinx <jin.xu@oracle.com>
2018-07-26 11:16:56 +0800

    examples-1

---
+++ examples/examples-1/.terraform/modules/7e1cfd6be67227853b963e00533d219e/variables.tf
@@ -0,0 +1,73 @@
+variable "compartment_ocid" {
+  description = "Compartment's OCID where VCN will be created. "
+
+}
+
+variable "availability_domains" {
+  description = "The Availability Domain of the instance. "
+  default     = []
+}
+
+variable "kafka_display_name" {
+  description = "The name of the elasticsearch instance. "
+  default     = ""
+}
+
+variable "subnet_ids" {
+  description = "The OCID of the master subnet to create the VNIC in. "
+  default     = []
+}
+
+variable "shape" {
+  description = "Instance shape to use for elastic search instance. "
+  default     = ""
+}
+
+
+variable "assign_public_ip" {
+  description = "Whether the VNIC should be assigned a public IP address. Default 'true' assigns a public IP address. "
+  default     = true
+}
+
+variable "ssh_authorized_keys" {
+  description = "Public SSH keys path to be included in the ~/.ssh/authorized_keys file for the default user on the instance. "
+  default     = ""
+}
+
+variable "ssh_private_key" {
+  description = "The private key path to access instance. "
+  default     = ""
+}
+
+variable "image_id" {
+  description = "The OCID of an image for an instance to use. "
+  default     = ""
+}
+
+variable "number_of_kafka" {
+  description = "The number of kafka instance(s) to create"
+}
+
+variable "cluster_name" {
+  default     = "kafka_cluster"
+}
+
+variable "zookeeper_client_port" {
+  description = "Zookeeper client port"
+
+}
+
+variable "zookeeper_internal_port" {
+  description = "Zookeeper internal port"
+
+}
+
+variable "zookeeper_poll_port" {
+  description = "Zookeeper poll port"
+
+}
+
+variable "kafka_client_port" {
+  description = "Kafka client port"
+  
+}
